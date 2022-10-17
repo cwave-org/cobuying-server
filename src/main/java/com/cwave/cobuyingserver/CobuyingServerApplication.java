d91f31f26ad10e5de063e9cfa202fc23f3abe6f4
summit45 <brandy2145@gmail.com>
2022-10-17 19:02:10 +0900

    [feat] base settings

---
+++ src/main/java/com/cwave/cobuyingserver/CobuyingServerApplication.java
@@ -0,0 +1,13 @@
+package com.cwave.cobuyingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobuyingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CobuyingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cwave/cobuyingserver/CobuyingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwave.cobuyingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobuyingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
