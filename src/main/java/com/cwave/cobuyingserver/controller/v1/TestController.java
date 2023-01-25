87b389e11013c427edd38060b3d2166e449010c8
summit45 <brandy2145@gmail.com>
2023-01-26 00:45:20 +0900

    [feat] react와 api 연결

--- src/main/java/com/cwave/cobuyingserver/controller/v1/TestController.java
+++ src/main/java/com/cwave/cobuyingserver/controller/v1/TestController.java
@@ -1,7 +1,6 @@
 package com.cwave.cobuyingserver.controller.v1;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,5 +11,17 @@ public class TestController {
     @GetMapping("/react") //리액트와 연결 확인
     public List<String> Hello(){
         return Arrays.asList("서버 포트는 8080", "리액트 포트는 3000");
+    }
+
+    @RequestMapping(value="/auth", method = {RequestMethod.POST})
+    public List<String> auth(@RequestBody String data){
+        System.out.println(data);
+        return Arrays.asList("인증 성공");
+    }
+
+    @RequestMapping(value="/", method = {RequestMethod.GET, RequestMethod.POST})
+    public List<String> auth2(@RequestBody String data){
+        System.out.println(data);
+        return Arrays.asList("인증 성공");
     }
 }
